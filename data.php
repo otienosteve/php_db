1353afb6441efc7531f684607866fa6d9f5f6f17
steveotieno <steveotieno701@gmail.com>
2021-08-30 23:00:45 +0300

    stored variable from db to php

--- data.php
+++ data.php
@@ -6,8 +6,12 @@ $id=$_POST['id'];
 
 echo "You entered the values ".$user." ".$passw." ".$level." ".$id;
 $conn=mysqli_connect('localhost','root','','school');
-$upd=mysqli_query($conn,"select level from users where id='$id';");
+$upd=mysqli_query($conn,"select * from users where id='$id';");
 $result=mysqli_fetch_row($upd);
 $result+=12;
 $upda=mysqli_query($conn,"update table users set level='$result' where id='$id';");
+$firstname=mysqli_fetch_assoc($upd);
+echo "<br>";
+echo $fistname=$firstname['level'];
+echo $fistname;
 //$add=mysqli_query($conn,"insert into users values ('$upd','$user','$passw','$level');");
